a7c82435c601ef53a6a73749e6a79474da0684a2
speedy-developer <indirtendo@gmail.com>
2022-01-03 21:49:36 +0300

    COMMIT

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function sub(){
 var fiy = document.getElementById("fiy").value;
 var kdv = document.getElementById("kdv").value;
 var son = fiy/(0.01*kdv+1);
-document.getElementById("son").innerHTML = son;
+document.getElementById("son").innerHTML = "Sonuç: "+son;
 }
 
